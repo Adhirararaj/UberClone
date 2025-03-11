aaf80e7a7fe7e4c537dcab03f7b90774ab0148e4
Adhirararaj <adhirajghadge11@gmail.com>
2025-03-11 18:35:12 +0530

    Updated app.js, user controller, and package files

---
+++ Backend/routes/user.routes.js
@@ -0,0 +1,22 @@
+const express= require('express');
+const router = express.Router();
+const {body} = require('express-validator');
+const userController = require('../controllers/user.controller');
+
+router.post('/register',[
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('fullname.firstname').isLength({min:3}).withMessage('Name is less than 3 charachters long '),
+    body('password').isLength({min:6}).withMessage('Password is too short ') 
+],
+    userController.registerUser
+)
+
+router.post('/login',[
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({min:6}).withMessage('Password is too short ')     
+],
+    userController.loginUser
+)
+
+
+module.exports= router; 
